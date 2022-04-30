712b133b1ebb6894052d77cc12bc6141f1d0a146
Daria Cook <nekonohimitsu@hotmail.com>
2022-04-30 13:09:18 -0400

    Create Nekonohimitsu_BLU.lua

---
+++ Nekonohimitsu_BLU.lua
@@ -0,0 +1,402 @@
+-------------------------------------------------------------------------------------------------------------------
+-- Setup functions for this job.  Generally should not be modified.
+-------------------------------------------------------------------------------------------------------------------
+
+-- Initialization function for this job file.
+function get_sets()
+    mote_include_version = 2
+    
+    -- Load and initialize the include file.
+    include('Mote-Include.lua')
+end
+
+
+-- Setup vars that are user-independent.  state.Buff vars initialized here will automatically be tracked.
+function job_setup()
+    state.Buff['Burst Affinity'] = buffactive['Burst Affinity'] or false
+    state.Buff['Chain Affinity'] = buffactive['Chain Affinity'] or false
+    state.Buff.Convergence = buffactive.Convergence or false
+    state.Buff.Diffusion = buffactive.Diffusion or false
+    state.Buff.Efflux = buffactive.Efflux or false
+    
+    state.Buff['Unbridled Learning'] = buffactive['Unbridled Learning'] or false
+
+
+    blue_magic_maps = {}
+    
+    -- Mappings for gear sets to use for various blue magic spells.
+    -- While Str isn't listed for each, it's generally assumed as being at least
+    -- moderately signficant, even for spells with other mods.
+    
+    -- Physical Spells --
+    
+    -- Physical spells with no particular (or known) stat mods
+    blue_magic_maps.Physical = S{
+        'Bilgestorm'
+    }
+
+    -- Spells with heavy accuracy penalties, that need to prioritize accuracy first.
+    blue_magic_maps.PhysicalAcc = S{
+        'Heavy Strike',
+    }
+
+    -- Physical spells with Str stat mod
+    blue_magic_maps.PhysicalStr = S{
+        'Battle Dance','Bloodrake','Death Scissors','Dimensional Death',
+        'Empty Thrash','Quadrastrike','Sinker Drill','Spinal Cleave',
+        'Uppercut','Vertical Cleave'
+    }
+        
+    -- Physical spells with Dex stat mod
+    blue_magic_maps.PhysicalDex = S{
+        'Amorphic Spikes','Asuran Claws','Barbed Crescent','Claw Cyclone','Disseverment',
+        'Foot Kick','Frenetic Rip','Goblin Rush','Hysteric Barrage','Paralyzing Triad',
+        'Seedspray','Sickle Slash','Smite of Rage','Terror Touch','Thrashing Assault',
+        'Vanity Dive'
+    }
+        
+    -- Physical spells with Vit stat mod
+    blue_magic_maps.PhysicalVit = S{
+        'Body Slam','Cannonball','Delta Thrust','Glutinous Dart','Grand Slam',
+        'Power Attack','Quad. Continuum','Sprout Smack','Sub-zero Smash'
+    }
+        
+    -- Physical spells with Agi stat mod
+    blue_magic_maps.PhysicalAgi = S{
+        'Benthic Typhoon','Feather Storm','Helldive','Hydro Shot','Jet Stream',
+        'Pinecone Bomb','Spiral Spin','Wild Oats'
+    }
+
+    -- Physical spells with Int stat mod
+    blue_magic_maps.PhysicalInt = S{
+        'Mandibular Bite','Queasyshroom'
+    }
+
+    -- Physical spells with Mnd stat mod
+    blue_magic_maps.PhysicalMnd = S{
+        'Ram Charge','Screwdriver','Tourbillion'
+    }
+
+    -- Physical spells with Chr stat mod
+    blue_magic_maps.PhysicalChr = S{
+        'Bludgeon'
+    }
+
+    -- Physical spells with HP stat mod
+    blue_magic_maps.PhysicalHP = S{
+        'Final Sting'
+    }
+
+    -- Magical Spells --
+
+    -- Magical spells with the typical Int mod
+    blue_magic_maps.Magical = S{
+        'Blastbomb','Blazing Bound','Bomb Toss','Cursed Sphere','Dark Orb','Death Ray',
+        'Diffusion Ray','Droning Whirlwind','Embalming Earth','Firespit','Foul Waters',
+        'Ice Break','Leafstorm','Maelstrom','Rail Cannon','Regurgitation','Rending Deluge',
+        'Retinal Glare','Subduction','Tem. Upheaval','Water Bomb'
+    }
+
+    -- Magical spells with a primary Mnd mod
+    blue_magic_maps.MagicalMnd = S{
+        'Acrid Stream','Evryone. Grudge','Magic Hammer','Mind Blast'
+    }
+
+    -- Magical spells with a primary Chr mod
+    blue_magic_maps.MagicalChr = S{
+        'Eyes On Me','Mysterious Light'
+    }
+
+    -- Magical spells with a Vit stat mod (on top of Int)
+    blue_magic_maps.MagicalVit = S{
+        'Thermal Pulse'
+    }
+
+    -- Magical spells with a Dex stat mod (on top of Int)
+    blue_magic_maps.MagicalDex = S{
+        'Charged Whisker','Gates of Hades'
+    }
+            
+    -- Magical spells (generally debuffs) that we want to focus on magic accuracy over damage.
+    -- Add Int for damage where available, though.
+    blue_magic_maps.MagicAccuracy = S{
+        '1000 Needles','Absolute Terror','Actinic Burst','Auroral Drape','Awful Eye',
+        'Blank Gaze','Blistering Roar','Blood Drain','Blood Saber','Chaotic Eye',
+        'Cimicine Discharge','Cold Wave','Corrosive Ooze','Demoralizing Roar','Digest',
+        'Dream Flower','Enervation','Feather Tickle','Filamented Hold','Frightful Roar',
+        'Geist Wall','Hecatomb Wave','Infrasonics','Jettatura','Light of Penance',
+        'Lowing','Mind Blast','Mortal Ray','MP Drainkiss','Osmosis','Reaving Wind',
+        'Sandspin','Sandspray','Sheep Song','Soporific','Sound Blast','Stinking Gas',
+        'Sub-zero Smash','Venom Shell','Voracious Trunk','Yawn'
+    }
+        
+    -- Breath-based spells
+    blue_magic_maps.Breath = S{
+        'Bad Breath','Flying Hip Press','Frost Breath','Heat Breath',
+        'Hecatomb Wave','Magnetite Cloud','Poison Breath','Radiant Breath','Self-Destruct',
+        'Thunder Breath','Vapor Spray','Wind Breath'
+    }
+
+    -- Stun spells
+    blue_magic_maps.Stun = S{
+        'Blitzstrahl','Frypan','Head Butt','Sudden Lunge','Tail slap','Temporal Shift',
+        'Thunderbolt','Whirl of Rage'
+    }
+        
+    -- Healing spells
+    blue_magic_maps.Healing = S{
+        'Healing Breeze','Magic Fruit','Plenilune Embrace','Pollen','Restoral','White Wind',
+        'Wild Carrot'
+    }
+    
+    -- Buffs that depend on blue magic skill
+    blue_magic_maps.SkillBasedBuff = S{
+        'Barrier Tusk','Diamondhide','Magic Barrier','Metallic Body','Plasma Charge',
+        'Pyric Bulwark','Reactor Cool',
+    }
+
+    -- Other general buffs
+    blue_magic_maps.Buff = S{
+        'Amplification','Animating Wail','Battery Charge','Carcharian Verve','Cocoon',
+        'Erratic Flutter','Exuviation','Fantod','Feather Barrier','Harden Shell',
+        'Memento Mori','Nat. Meditation','Occultation','Orcish Counterstance','Refueling',
+        'Regeneration','Saline Coat','Triumphant Roar','Warm-Up','Winds of Promyvion',
+        'Zephyr Mantle'
+    }
+    
+    
+    -- Spells that require Unbridled Learning to cast.
+    unbridled_spells = S{
+        'Absolute Terror','Bilgestorm','Blistering Roar','Bloodrake','Carcharian Verve',
+        'Crashing Thunder','Droning Whirlwind','Gates of Hades','Harden Shell','Polar Roar',
+        'Pyric Bulwark','Thunderbolt','Tourbillion','Uproot'
+    }
+end
+
+-------------------------------------------------------------------------------------------------------------------
+-- User setup functions for this job.  Recommend that these be overridden in a sidecar file.
+-------------------------------------------------------------------------------------------------------------------
+
+-- Setup vars that are user-dependent.  Can override this function in a sidecar file.
+function user_setup()
+    state.OffenseMode:options('Normal', 'Acc', 'Learning')
+	state.HybridMode:options('Normal', 'DT')
+    state.WeaponskillMode:options('Normal', 'Acc')
+    state.CastingMode:options('Normal', 'Resistant')
+    state.IdleMode:options('Normal', 'Refresh', 'Regen', 'Learning')
+end
+
+-- Set up gear sets.
+function init_gear_sets()
+    define_spell_and_ja_sets()
+	define_ws_sets()
+	define_engaged_sets()
+	define_idle_dt_sets()  
+end
+
+function define_spell_and_ja_sets()
+--------------------------------------
+    -- Start defining the sets
+    --------------------------------------
+
+    sets.buff['Burst Affinity'] = {}
+    sets.buff['Chain Affinity'] = {}
+    sets.buff.Convergence = {}
+    sets.buff.Diffusion = {}
+    sets.buff.Enchainment = {}
+    sets.buff.Efflux = {}
+
+    
+    -- Precast Sets
+    
+    -- Precast sets to enhance JAs
+    sets.precast.JA['Azure Lore'] = {}
+ 
+    -- Fast cast sets for spells
+    sets.precast.FC = {}
+    
+    -- Midcast Sets
+    sets.midcast.FastRecast = {}
+        
+    sets.midcast['Blue Magic'] = {}
+    
+    -- Physical Spells --
+    
+    sets.midcast['Blue Magic'].Physical = {}
+
+    sets.midcast['Blue Magic'].PhysicalAcc = {}
+
+    sets.midcast['Blue Magic'].PhysicalStr = set_combine(sets.midcast['Blue Magic'].Physical, {})
+
+    sets.midcast['Blue Magic'].PhysicalDex = set_combine(sets.midcast['Blue Magic'].Physical, {})
+
+    sets.midcast['Blue Magic'].PhysicalVit = set_combine(sets.midcast['Blue Magic'].Physical, {})
+
+    sets.midcast['Blue Magic'].PhysicalAgi = set_combine(sets.midcast['Blue Magic'].Physical, {})
+
+    sets.midcast['Blue Magic'].PhysicalInt = set_combine(sets.midcast['Blue Magic'].Physical, {})
+
+    sets.midcast['Blue Magic'].PhysicalMnd = set_combine(sets.midcast['Blue Magic'].Physical, {})
+
+    sets.midcast['Blue Magic'].PhysicalChr = set_combine(sets.midcast['Blue Magic'].Physical, {})
+
+    sets.midcast['Blue Magic'].PhysicalHP = set_combine(sets.midcast['Blue Magic'].Physical, {})
+
+
+    -- Magical Spells --
+    
+    sets.midcast['Blue Magic'].Magical = {}
+
+    sets.midcast['Blue Magic'].Magical.Resistant = set_combine(sets.midcast['Blue Magic'].Magical, {})
+    
+    sets.midcast['Blue Magic'].MagicalMnd = set_combine(sets.midcast['Blue Magic'].Magical, {})
+
+    sets.midcast['Blue Magic'].MagicalChr = set_combine(sets.midcast['Blue Magic'].Magical, {})
+
+    sets.midcast['Blue Magic'].MagicalVit = set_combine(sets.midcast['Blue Magic'].Magical, {})
+
+    sets.midcast['Blue Magic'].MagicalDex = set_combine(sets.midcast['Blue Magic'].Magical, {})
+
+    sets.midcast['Blue Magic'].MagicAccuracy = {}
+
+    -- Breath Spells --
+    
+    sets.midcast['Blue Magic'].Breath = {}
+
+    -- Other Types --
+    
+    sets.midcast['Blue Magic'].Stun = set_combine(sets.midcast['Blue Magic'].MagicAccuracy, {})
+        
+    sets.midcast['Blue Magic']['White Wind'] = {}
+
+    sets.midcast['Blue Magic'].Healing = {}
+
+    sets.midcast['Blue Magic'].SkillBasedBuff = {}
+
+    sets.midcast['Blue Magic'].Buff = {}
+    
+    sets.midcast.Protect = {ring1="Sheltered Ring"}
+    sets.midcast.Protectra = {ring1="Sheltered Ring"}
+    sets.midcast.Shell = {ring1="Sheltered Ring"}
+    sets.midcast.Shellra = {ring1="Sheltered Ring"}
+    
+    sets.self_healing = {}
+end
+function define_ws_sets()
+    -- Weaponskill sets
+    -- Default set for any weaponskill that isn't any more specifically defined
+    sets.precast.WS = {}
+    
+    sets.precast.WS.acc = set_combine(sets.precast.WS, {})
+
+    -- Specific weaponskill sets.  Uses the base set if an appropriate WSMod version isn't found.
+    sets.precast.WS['Requiescat'] = set_combine(sets.precast.WS, {})
+
+    sets.precast.WS['Sanguine Blade'] = {}
+end
+function define_engaged_sets()
+    sets.engaged = {}
+
+    sets.engaged.Acc = {}
+
+    sets.engaged.DW = {}
+
+    sets.engaged.DW.Acc = {}
+
+    sets.engaged.Learning = set_combine(sets.engaged, sets.Learning)
+    sets.engaged.DW.Learning = set_combine(sets.engaged.DW, sets.Learning)
+end
+function define_idle_dt_sets() 
+
+    -- Gear for learning spells: +skill and AF hands.
+    sets.Learning = {}
+
+    sets.latent_refresh = {waist="Fucho-no-obi"}
+	
+    -- Defense sets
+    sets.defense.PDT = {}
+
+    sets.defense.MDT = set_combine(sets.defense.PDT, {})
+    
+    -- Idle sets
+    sets.idle = set_combine(sets.defense.PDT, {})
+
+    sets.idle.PDT = set_combine(sets.idle, {})
+
+    sets.idle.Learning = set_combine(sets.idle, sets.Learning)
+
+    sets.Kiting = {legs="Carmine Cuisses +1"}
+end 
+
+-------------------------------------------------------------------------------------------------------------------
+-- Job-specific hooks for standard casting events.
+-------------------------------------------------------------------------------------------------------------------
+
+-- Set eventArgs.handled to true if we don't want any automatic gear equipping to be done.
+-- Set eventArgs.useMidcastGear to true if we want midcast gear equipped on precast.
+function job_precast(spell, action, spellMap, eventArgs)
+    if unbridled_spells:contains(spell.english) and not state.Buff['Unbridled Learning'] then
+        eventArgs.cancel = true
+        windower.send_command('@input /ja "Unbridled Learning" <me>; wait 1.5; input /ma "'..spell.name..'" '..spell.target.name)
+    end
+end
+
+-- Run after the default midcast() is done.
+-- eventArgs is the same one used in job_midcast, in case information needs to be persisted.
+function job_post_midcast(spell, action, spellMap, eventArgs)
+    -- Add enhancement gear for Chain Affinity, etc.
+    if spell.skill == 'Blue Magic' then
+        for buff,active in pairs(state.Buff) do
+            if active and sets.buff[buff] then
+                equip(sets.buff[buff])
+            end
+        end
+        if spellMap == 'Healing' and spell.target.type == 'SELF' and sets.self_healing then
+            equip(sets.self_healing)
+        end
+    end
+
+    -- If in learning mode, keep on gear intended to help with that, regardless of action.
+    if state.OffenseMode.value == 'Learning' then
+        equip(sets.Learning)
+    end
+end
+
+
+-------------------------------------------------------------------------------------------------------------------
+-- Job-specific hooks for non-casting events.
+-------------------------------------------------------------------------------------------------------------------
+
+-- Called when a player gains or loses a buff.
+-- buff == buff gained or lost
+-- gain == true if the buff was gained, false if it was lost.
+function job_buff_change(buff, gain)
+    if state.Buff[buff] ~= nil then
+        state.Buff[buff] = gain
+    end
+end
+
+-------------------------------------------------------------------------------------------------------------------
+-- User code that supplements standard library decisions.
+-------------------------------------------------------------------------------------------------------------------
+
+-- Custom spell mapping.
+-- Return custom spellMap value that can override the default spell mapping.
+-- Don't return anything to allow default spell mapping to be used.
+function job_get_spell_map(spell, default_spell_map)
+    if spell.skill == 'Blue Magic' then
+        for category,spell_list in pairs(blue_magic_maps) do
+            if spell_list:contains(spell.english) then
+                return category
+            end
+        end
+    end
+end
+
+-- Modify the default idle set after it was constructed.
+function customize_idle_set(idleSet)
+    if player.mpp < 51 then
+        set_combine(idleSet, sets.latent_refresh)
+    end
+    return idleSet
+end
